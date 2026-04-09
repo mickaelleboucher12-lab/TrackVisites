2b18aa030c6dfd38e53500aa3d893b3a7d6632da
mleboucher <mleboucher@user.antigravity>
2026-04-09 09:59:45 +0200

    Mise Ã  jour TrackVisites - 2026-04-09 09:59

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configurer ici vos accès Supabase
-const SUPABASE_URL = 'https://gbtomeykpoxcqprsgkfa.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdidG9tZXlrcG94Y3FwcnNna2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1ODgwNjgsImV4cCI6MjA4ODE2NDA2OH0.ta-eHx4FLWV6kcT_K1p0t6zPQ7dwiLpzXo3S3qqcWr4';
+const SUPABASE_URL = 'https://rxulxpeiqmuunnlspmvd.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4dWx4cGVpcW11dW5ubHNwbXZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MTQxNzQsImV4cCI6MjA5MTI5MDE3NH0.yAk6IBuIF3PkrRug8OncwLlFFNxK0cNG9hT5uKfmMc4';
 
 const supabase = lib.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
